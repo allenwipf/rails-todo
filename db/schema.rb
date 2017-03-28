c36a6a141d59c7f33fc2e863df38c2aaa15b2ab8
Allen Wipf <allenwipf@msn.com>
2017-03-28 11:32:33 -0500

    changed database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170325224837) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lists", force: :cascade do |t|
     t.string   "title"
     t.boolean  "completed"
